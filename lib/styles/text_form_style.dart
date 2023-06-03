6c80dbc63aed2d1b36cb893ce3e91f11866d53fa
sora-k46 <sorrawit456789@gmail.com>
2023-06-03 17:01:04 +0700

    Create text_form_style.dart

---
+++ lib/styles/text_form_style.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+
+final formBorder = OutlineInputBorder(
+  borderSide: const BorderSide(width: 1, color: Colors.grey),
+  borderRadius: BorderRadius.circular(8),
+);
