5ce1955a28d44f889db081d10f50c3882782708e
sora-k46 <sorrawit456789@gmail.com>
2023-06-11 00:08:27 +0700

    Create profile_pic.dart

---
+++ lib/widgets/profile_pic.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class ProfilePic extends StatelessWidget {
+  const ProfilePic({Key? key, this.image}) : super(key: key);
+  final String? image;
+
+  @override
+  Widget build(BuildContext context) {
+    return image == null
+        ? const CircleAvatar(
+            backgroundColor: Colors.green,
+            child: Icon(Icons.person, color: Colors.white, size: 100),
+          )
+        : CircleAvatar(
+            backgroundImage: NetworkImage(image!),
+          );
+  }
+}
