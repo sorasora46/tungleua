c326c417e340baa2014abedb7bec06b8e4aa9c9a
sora-k46 <sorrawit456789@gmail.com>
2023-06-12 22:54:08 +0700

    Create store model

---
+++ lib/models/store.dart
@@ -0,0 +1,67 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+
+class Store {
+  final String id; // store_id
+  final String name;
+  final String contact;
+  final String timeOpen;
+  final String timeClose;
+  final String description;
+  final double latitude;
+  final double longtitude;
+  final String userId;
+  final List<Uint8List> images;
+
+  Store(
+      {required this.id,
+      required this.name,
+      required this.contact,
+      required this.timeOpen,
+      required this.timeClose,
+      required this.description,
+      required this.latitude,
+      required this.longtitude,
+      required this.userId,
+      required this.images});
+
+  Map<String, dynamic> toJSON() {
+    return {
+      'id': id,
+      'name': name,
+      'contact': contact,
+      'time_open': timeOpen,
+      'time_close': timeClose,
+      'description': description,
+      'latitude': latitude,
+      'longitude': longtitude,
+      'user_id': userId,
+      'images': images,
+    };
+  }
+
+  factory Store.fromJSON(Map<String, dynamic> json) {
+    return Store(
+      id: json['id'] as String,
+      name: json['name'] as String,
+      contact: json['contact'] as String,
+      timeOpen: json['time_open'] as String,
+      timeClose: json['time_close'] as String,
+      description: json['description'] as String,
+      latitude: json['latitude'] as double,
+      longtitude: json['longtitude'] as double,
+      userId: json['user_id'] as String,
+      images: (json['images'] as List<dynamic>)
+          .map((storeImage) => base64Decode(storeImage['Image']))
+          .toList(),
+    );
+  }
+
+  @override
+  String toString() {
+    return 'Store{id: $id, name: $name, contact: $contact, timeOpen: $timeOpen, '
+        'timeClose: $timeClose, description: $description, latitude: $latitude, '
+        'longitude: $longtitude, userId: $userId}';
+  }
+}
