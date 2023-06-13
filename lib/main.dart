705456583e13617aa3b6140c7faf32a48ab5a286
Nattapat-is-my-name <bossnattapat238@gmail.com>
2023-06-14 05:48:55 +0700

    Revert "Revert "Revert "init firebase , complete login ,not complete register and logout"""
    This reverts commit d08df233e5de4cf01510497854ddb662e5628a92.

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:tungleua/app.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
 
-Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+void main() {
   runApp(const MainApp());
 }
 
