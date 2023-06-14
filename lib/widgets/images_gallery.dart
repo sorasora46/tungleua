53f515fbc2ad5a874216162d987e0a2e9898a442
sora-k46 <sorrawit456789@gmail.com>
2023-06-14 16:19:19 +0700

    Change image placeholder to get rid of error

--- lib/widgets/images_gallery.dart
+++ lib/widgets/images_gallery.dart
@@ -4,8 +4,7 @@ class ImagesGallery extends StatelessWidget {
   const ImagesGallery({Key? key, required this.images}) : super(key: key);
   final List<String> images;
 
-  final shopPic =
-      'https://scontent.fbkk5-5.fna.fbcdn.net/v/t39.30808-6/240585651_972435579980123_8072977601646520572_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeFX_8K7-vKC5SlN1CIzX3o1YjiUm-tYyBFiOJSb61jIEUlPRTmC-8U4KFYqVxBEc5BpybJcgdnaLje_ngBUdcHo&_nc_ohc=A0upwk9UygEAX-MWT_L&_nc_ht=scontent.fbkk5-5.fna&oh=00_AfAyd3GTlDufsbsQOdb_eEufqZ0OCNCH_8wAB6nR4ZMN0g&oe=648242C5';
+  final shopPic = 'https://placehold.co/600x400';
 
   @override
   Widget build(BuildContext context) {
