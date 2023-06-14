8f16def5e4f545c3a0a681259d38d65b231a2789
sora-k46 <sorrawit456789@gmail.com>
2023-06-14 15:08:18 +0700

    Convert ShopBottomSheet to Stateful

--- lib/widgets/shop_bottom_sheet.dart
+++ lib/widgets/shop_bottom_sheet.dart
@@ -2,9 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:tungleua/pages/shop_detail.dart';
 
 // TODO: Take Shop or Shop detail as argument for render bottom sheet
-class ShopBottomSheet extends StatelessWidget {
+class ShopBottomSheet extends StatefulWidget {
   const ShopBottomSheet({Key? key}) : super(key: key);
 
+  @override
+  State<ShopBottomSheet> createState() => _ShopBottomSheetState();
+}
+
+class _ShopBottomSheetState extends State<ShopBottomSheet> {
   final shopPic =
       'https://scontent.fbkk5-5.fna.fbcdn.net/v/t39.30808-6/240585651_972435579980123_8072977601646520572_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeFX_8K7-vKC5SlN1CIzX3o1YjiUm-tYyBFiOJSb61jIEUlPRTmC-8U4KFYqVxBEc5BpybJcgdnaLje_ngBUdcHo&_nc_ohc=A0upwk9UygEAX-MWT_L&_nc_ht=scontent.fbkk5-5.fna&oh=00_AfAyd3GTlDufsbsQOdb_eEufqZ0OCNCH_8wAB6nR4ZMN0g&oe=648242C5';
 
