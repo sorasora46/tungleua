a515c2f79c0e55a895f16c96e721a28dd9279432
sora-k46 <sorrawit456789@gmail.com>
2023-06-14 15:57:14 +0700

    Remove unused image

--- lib/pages/shop_detail.dart
+++ lib/pages/shop_detail.dart
@@ -16,9 +16,6 @@ class ShopDetail extends StatefulWidget {
 }
 
 class _ShopDetailState extends State<ShopDetail> {
-  final shopPic =
-      'https://scontent.fbkk5-5.fna.fbcdn.net/v/t39.30808-6/240585651_972435579980123_8072977601646520572_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=e3f864&_nc_eui2=AeFX_8K7-vKC5SlN1CIzX3o1YjiUm-tYyBFiOJSb61jIEUlPRTmC-8U4KFYqVxBEc5BpybJcgdnaLje_ngBUdcHo&_nc_ohc=A0upwk9UygEAX-MWT_L&_nc_ht=scontent.fbkk5-5.fna&oh=00_AfAyd3GTlDufsbsQOdb_eEufqZ0OCNCH_8wAB6nR4ZMN0g&oe=648242C5';
-
   List<Product>? products;
 
   @override
