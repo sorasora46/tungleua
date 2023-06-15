4386f2aa5506a5fdc4b26c280c468739c5c71112
sora-k46 <sorrawit456789@gmail.com>
2023-06-16 01:09:33 +0700

    Render product images

--- lib/widgets/images_gallery.dart
+++ lib/widgets/images_gallery.dart
@@ -1,47 +1,42 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:tungleua/services/store_service.dart';
+import 'package:tungleua/widgets/rounded_image.dart';
 
-class ImagesGallery extends StatelessWidget {
-  const ImagesGallery({Key? key, required this.images}) : super(key: key);
-  final List<String> images;
+class ImagesGallery extends StatefulWidget {
+  const ImagesGallery({Key? key, required this.storeId}) : super(key: key);
+  final String storeId;
 
-  final shopPic = 'https://placehold.co/600x400';
+  @override
+  State<ImagesGallery> createState() => _ImagesGalleryState();
+}
+
+class _ImagesGalleryState extends State<ImagesGallery> {
+  List<String>? images;
+
+  @override
+  void initState() {
+    super.initState();
+    StoreService()
+        .getStoreProductImages(widget.storeId)
+        .then((images) => setState(() {
+              this.images = images;
+            }));
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Row(children: <Widget>[
-      SizedBox(
-        height: 150,
-        width: 100,
-        child: ClipRRect(
-            borderRadius: BorderRadius.circular(8.0),
-            child: Image.network(shopPic, fit: BoxFit.cover)),
-      ),
-      const SizedBox(width: 10),
-      Column(children: <Widget>[
-        SizedBox(
-          height: 70,
-          width: 100,
-          child: ClipRRect(
-              borderRadius: BorderRadius.circular(8.0),
-              child: Image.network(shopPic, fit: BoxFit.cover)),
-        ),
-        const SizedBox(height: 10),
-        SizedBox(
-          height: 70,
-          width: 100,
-          child: ClipRRect(
-              borderRadius: BorderRadius.circular(8.0),
-              child: Image.network(shopPic, fit: BoxFit.cover)),
-        ),
-      ]),
-      const SizedBox(width: 10),
-      SizedBox(
-        height: 150,
-        width: 100,
-        child: ClipRRect(
-            borderRadius: BorderRadius.circular(8.0),
-            child: Image.network(shopPic, fit: BoxFit.cover)),
-      ),
-    ]);
+    if (images != null) {
+      return Row(
+        children: images!
+            .map((image) => Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: RoundedImage(image: base64Decode(image)),
+                ))
+            .toList(),
+      );
+    }
+    return const Center(child: CircularProgressIndicator());
   }
 }
