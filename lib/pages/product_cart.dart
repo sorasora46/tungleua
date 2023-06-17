2f335a1288b2a9d23ba05bb12149c482dc840ed3
sora-k46 <sorrawit456789@gmail.com>
2023-06-17 20:22:42 +0700

    Format code , handle select coupon page

--- lib/pages/product_cart.dart
+++ lib/pages/product_cart.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tungleua/models/product.dart';
+import 'package:tungleua/pages/discount_code.dart';
 import 'package:tungleua/widgets/cart_item.dart';
 
 class ProductCart extends StatefulWidget {
@@ -15,132 +16,125 @@ class _ProductCartState extends State<ProductCart> {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          automaticallyImplyLeading: false,
-          title: const Text('Cart'),
-          centerTitle: true,
-        ),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-              child: SingleChildScrollView(
-                child: Padding(
+            automaticallyImplyLeading: false,
+            title: const Text('Cart'),
+            centerTitle: true),
+        body: Column(children: <Widget>[
+          Expanded(
+            child: SingleChildScrollView(
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 0),
+                child: Column(children: <Widget>[
+                  CartItem(
+                      product: Product(
+                          id: "id",
+                          title: "title",
+                          description: "description",
+                          price: 20,
+                          storeId: "storeId",
+                          image: "11114444",
+                          amount: 50)),
+                  CartItem(
+                      product: Product(
+                          id: "id",
+                          title: "title",
+                          description: "description",
+                          price: 20,
+                          storeId: "storeId",
+                          image: "11114444",
+                          amount: 50)),
+                  CartItem(
+                      product: Product(
+                          id: "id",
+                          title: "title",
+                          description: "description",
+                          price: 20,
+                          storeId: "storeId",
+                          image: "11114444",
+                          amount: 50)),
+                  CartItem(
+                      product: Product(
+                          id: "id",
+                          title: "title",
+                          description: "description",
+                          price: 20,
+                          storeId: "storeId",
+                          image: "11114444",
+                          amount: 50)),
+                  CartItem(
+                      product: Product(
+                          id: "id",
+                          title: "title",
+                          description: "description",
+                          price: 20,
+                          storeId: "storeId",
+                          image: "11114444",
+                          amount: 50)),
+                  CartItem(
+                      product: Product(
+                          id: "id",
+                          title: "title",
+                          description: "description",
+                          price: 20,
+                          storeId: "storeId",
+                          image: "11114444",
+                          amount: 50))
+                ]),
+              ),
+            ),
+          ),
+
+          // Code
+          Container(
+              decoration: BoxDecoration(
+                  border: Border.symmetric(
+                      vertical: BorderSide.none,
+                      horizontal:
+                          BorderSide(width: 1, color: Colors.grey.shade300))),
+              child: Padding(
                   padding:
-                      const EdgeInsets.symmetric(horizontal: 16, vertical: 0),
-                  child: Column(
-                    children: [
-                      CartItem(
-                          product: Product(
-                              id: "id",
-                              title: "title",
-                              description: "description",
-                              price: 20,
-                              storeId: "storeId",
-                              image: "11114444",
-                              amount: 50)),
-                      CartItem(
-                          product: Product(
-                              id: "id",
-                              title: "title",
-                              description: "description",
-                              price: 20,
-                              storeId: "storeId",
-                              image: "11114444",
-                              amount: 50)),
-                      CartItem(
-                          product: Product(
-                              id: "id",
-                              title: "title",
-                              description: "description",
-                              price: 20,
-                              storeId: "storeId",
-                              image: "11114444",
-                              amount: 50)),
-                      CartItem(
-                          product: Product(
-                              id: "id",
-                              title: "title",
-                              description: "description",
-                              price: 20,
-                              storeId: "storeId",
-                              image: "11114444",
-                              amount: 50)),
-                      CartItem(
-                          product: Product(
-                              id: "id",
-                              title: "title",
-                              description: "description",
-                              price: 20,
-                              storeId: "storeId",
-                              image: "11114444",
-                              amount: 50)),
-                      CartItem(
-                          product: Product(
-                              id: "id",
-                              title: "title",
-                              description: "description",
-                              price: 20,
-                              storeId: "storeId",
-                              image: "11114444",
-                              amount: 50)),
-                    ],
-                  ),
-                ),
-              ),
-            ),
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 0),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        const Text('Code',
+                            style: TextStyle(
+                                fontSize: 14, fontWeight: FontWeight.w500)),
+                        TextButton(
+                            onPressed: () => Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) =>
+                                        const DiscountCode())),
+                            style: const ButtonStyle(
+                                splashFactory: NoSplash.splashFactory),
+                            child: const Row(children: [
+                              Text('Select Code',
+                                  style: TextStyle(fontSize: 14)),
+                              Icon(Icons.arrow_forward_ios, size: 12)
+                            ]))
+                      ]))),
 
-            // Code
-            Container(
+          // Pay
+          Container(
               decoration: BoxDecoration(
                   border: Border.symmetric(
                       vertical: BorderSide.none,
                       horizontal:
                           BorderSide(width: 1, color: Colors.grey.shade300))),
               child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 10, vertical: 0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    const Text('Code',
-                        style: TextStyle(
-                            fontSize: 14, fontWeight: FontWeight.w500)),
-                    TextButton(
-                      onPressed: () {},
-                      child: const Row(
-                        children: [
-                          Text('Select Code', style: TextStyle(fontSize: 14)),
-                          Icon(Icons.arrow_forward_ios, size: 12),
-                        ],
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-
-            // Pay
-            Container(
-              decoration: BoxDecoration(
-                  border: Border.symmetric(
-                      vertical: BorderSide.none,
-                      horizontal:
-                          BorderSide(width: 1, color: Colors.grey.shade300))),
-              child: Padding(
-                padding:
-                    const EdgeInsets.symmetric(horizontal: 10, vertical: 0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: <Widget>[
-                    const Text('Total: ฿ ${260}',
-                        style: TextStyle(
-                            fontSize: 14, fontWeight: FontWeight.w500)),
-                    FilledButton(onPressed: () {}, child: const Text('Pay')),
-                  ],
-                ),
-              ),
-            ),
-          ],
-        ),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 0),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        const Text('Total: ฿ ${260}',
+                            style: TextStyle(
+                                fontSize: 14, fontWeight: FontWeight.w500)),
+                        FilledButton(onPressed: () {}, child: const Text('Pay'))
+                      ])))
+        ]),
       ),
     );
   }
