8bda5dc8bcbf16af154fb9441d6158564decdcdd
sora-k46 <sorrawit456789@gmail.com>
2023-06-18 21:44:31 +0700

    Create getCartItems api call

--- lib/services/cart_service.dart
+++ lib/services/cart_service.dart
@@ -1,3 +1,4 @@
+import 'package:tungleua/models/cart_item.dart';
 import 'package:tungleua/services/api.dart';
 
 class CartService {
@@ -5,5 +6,18 @@ class CartService {
       String userId, String productId, int amount) async {
     await Api().dio.post(
         "/carts/add?user_id=$userId&product_id=$productId&amount=$amount");
+  }
+
+  Future<List<CartItem>?> getCartItems(String userId) async {
+    final response = await Api().dio.get("/carts/find-by-id/$userId");
+    if (response.statusCode != 200) {
+      return null;
+    }
+    final rawData = response.data as Map<String, dynamic>;
+    final results = rawData['results'] as List<dynamic>;
+    final items = results
+        .map((item) => CartItem.fromJSON(item as Map<String, dynamic>))
+        .toList();
+    return items;
   }
 }
