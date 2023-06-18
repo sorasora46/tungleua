6748c12868bcf1403579530b75a1d8b505db0e12
sora-k46 <sorrawit456789@gmail.com>
2023-06-19 00:35:19 +0700

    Handle center current location with current zoom level

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -160,7 +160,7 @@ class _HomeState extends State<Home> {
                     IconButton(
                       onPressed: () {
                         setState(() {
-                          mapController.move(currentLocation!, initZoom);
+                          mapController.move(currentLocation!, currentZoom);
                         });
                       },
                       icon: const Icon(Icons.location_searching),
