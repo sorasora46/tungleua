e85dec1e2b7d17e66ec9144ed90ca59ff3dfa5db
Nattapat-is-my-name <bossnattapat238@gmail.com>
2023-06-29 16:29:29 +0700

    fix login bug

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/foundation.dart';
+
 import 'package:tungleua/services/api.dart';
+import 'package:flutter/material.dart';
 
 class AuthService {
   final FirebaseAuth auth = FirebaseAuth.instance;
@@ -38,13 +39,23 @@ class AuthService {
   }
 
   Future<UserCredential?> signInWithEmailAndPassword(
-      String email, String password) async {
+      String email, String password, BuildContext context) async {
     try {
       final response = await Api()
           .dio
           .post('/auth/login', data: {'email': email, 'password': password});
       if (response.statusCode == 200) {
         final token = response.data['token'];
+        UserCredential userCredential =
+            await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: email,
+          password: password,
+        );
+        // User login successful
+        User? user = userCredential.user;
+
+        print('User logged in: ${user?.uid}');
+
         final userCred = await auth.signInWithCustomToken(token);
         // debugPrint(await auth.currentUser?.getIdToken());
         return userCred;
@@ -54,6 +65,12 @@ class AuthService {
       }
     } catch (e) {
       debugPrint(e.toString());
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text(
+              'The password is invalid or the user does not have a password.'),
+        ),
+      );
       return null;
     }
   }
