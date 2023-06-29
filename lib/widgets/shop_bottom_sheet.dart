b7064eb2fe21c6c285c6633456dfb4cfa138bf93
Neng Sitiporn <sitiporn.wimo@kmutt.ac.th>
2023-06-29 21:22:58 +0700

    handle gesture
    demo

--- lib/widgets/shop_bottom_sheet.dart
+++ lib/widgets/shop_bottom_sheet.dart
@@ -25,63 +25,62 @@ class _ShopBottomSheetState extends State<ShopBottomSheet> {
               if (snapshot.connectionState == ConnectionState.done) {
                 final Store store = snapshot.data!;
 
-                return Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: <Widget>[
-                      const SizedBox(height: 25),
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ShopDetail(store: store)));
+                  },
+                  child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        const SizedBox(height: 25),
 
-                      // Shop Name
-                      // TODO: Handle Authorization (Shop's Owner, Shop's Customer)
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) =>
-                                      ShopDetail(store: store)));
-                        },
-                        child: Text(
+                        // Shop Name
+                        // TODO: Handle Authorization (Shop's Owner, Shop's Customer)
+                        Text(
                           store.name,
                           style: const TextStyle(
                             fontSize: 23,
                             fontWeight: FontWeight.w500,
                           ),
                         ),
-                      ),
 
-                      const SizedBox(height: 10),
+                        const SizedBox(height: 10),
 
-                      // Shop open - close
-                      Text(
-                        '${store.timeOpen} - ${store.timeClose}',
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w400,
+                        // Shop open - close
+                        Text(
+                          'open on ${store.timeOpen} - ${store.timeClose}',
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w400,
+                          ),
                         ),
-                      ),
 
-                      const SizedBox(height: 10),
+                        const SizedBox(height: 10),
 
-                      // Shop Address (Description)
-                      Text(
-                        store.description,
-                        style: const TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.w300,
+                        // Shop Address (Description)
+                        Text(
+                          store.description,
+                          style: const TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w300,
+                          ),
                         ),
-                      ),
 
-                      const SizedBox(height: 30),
+                        const SizedBox(height: 30),
 
-                      // Pictures of shop's products
-                      // TODO: Handle render product images
-                      Center(
-                        child: SingleChildScrollView(
-                          scrollDirection: Axis.horizontal,
-                          child: ImagesGallery(storeId: store.id),
-                        ),
-                      )
-                    ]);
+                        // Pictures of shop's products
+                        // TODO: Handle render product images
+                        Center(
+                          child: SingleChildScrollView(
+                            scrollDirection: Axis.horizontal,
+                            child: ImagesGallery(storeId: store.id),
+                          ),
+                        )
+                      ]),
+                );
               } else {
                 return const Center(child: CircularProgressIndicator());
               }
